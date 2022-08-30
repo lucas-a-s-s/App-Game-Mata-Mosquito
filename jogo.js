2403e39897855c0a3dee878eb5451fa0ec4e29d1
lucas-a-s-s <lucas.-2010@hotmail.com>
2022-08-29 21:55:52 -0300

    Commit aula 229

--- jogo.js
+++ jogo.js
@@ -14,6 +14,12 @@ ajustaTamanhoPalcoJogo()
 
 function posicaoRandomica() {
 
+
+	//remover o mosquito anterior (caso exista)
+	if(document.getElementById('mosquito')) {
+		document.getElementById('mosquito').remove()
+	}
+
 	var posicaoX = Math.floor(Math.random() * largura) - 90
 	var posicaoY = Math.floor(Math.random() * altura) - 90
 
@@ -29,6 +35,7 @@ function posicaoRandomica() {
 	mosquito.style.left = posicaoX + 'px'
 	mosquito.style.top = posicaoY + 'px'
 	mosquito.style.position = 'absolute'
+	mosquito.id = 'mosquito'
 
 	document.body.appendChild(mosquito)
 
@@ -58,5 +65,7 @@ function ladoAleatorio() {
 		
 		case 1:
 			return 'ladoB'
+
 	}
 }
+
